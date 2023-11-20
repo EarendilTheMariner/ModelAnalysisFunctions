abda9446ae34c5839415e4ba532fbf06afe6ba27
EarendilTheMariner <august.w.i@live.dk>
2023-11-20 22:15:12 +0100

    function edits

--- speedControl.m
+++ speedControl.m
@@ -3,19 +3,20 @@ function MouseNet = speedControl(varargin)
 %% Oscillation frequency, input dependent recruitment of populations
     
     NP = NetworkParameters();
-    NP.AddCelltype('V2a-1','cau','','','','','',1000,0.1000*5);
+    NP.AddCelltype('V2a-1','cau','','','','','',1000,0.1000*2.5);
     NP.AddCelltype('V2a-2','bi','','','','','',500,0.1000*2.5);
     NP.AddCelltype('V1','bi','','','','','',2000,0.1000*2.5);
     MouseNet = Network(3000,0.5,NP);
     
     W = MouseNet.ConnMat;
 
-    simTime = 30000;
-    lowerIe = 20;
-    upperIe = 30;
-    V2a_1Thr = 25;
-    V2a_2Thr = 22.5;
+    simTime = 34000;
+    lowerIe = 17;
+    upperIe = 21;
+    V2a_1Thr = 21;
+    V2a_2Thr = 20.5;
     V1_Thr = 20;
+    caudInp = false;
 
         for ii = 1:2:length(varargin)
             switch varargin{ii}
@@ -31,6 +32,8 @@ function MouseNet = speedControl(varargin)
                     V2a_2Thr = varargin{ii+1};
                 case 'V1Threshold'
                     V1_Thr = varargin{ii+1};
+                case 'CaudalInput'
+                    caudInp = varargin{ii+1};
             end
          end
 
@@ -40,18 +43,30 @@ function MouseNet = speedControl(varargin)
 
     threshold = ones(size(W,1),1);
     threshold(V2a_1Ix) = V2a_1Thr;
-    threshold(V2a_2Ix) =  V2a_2Thr;
+    threshold(V2a_2Ix) = V2a_2Thr;
     threshold(V1Ix) = V1_Thr;
 
-    inputSignal = zeros(size(W,1),simTime);
-    inputSignal(:,1:5000) = lowerIe;
-    inputSignal(:,5000:9999) = repmat(linspace(lowerIe,upperIe,5000),size(W,1),1);
-    inputSignal(:,10000:20000) = upperIe;
-    inputSignal(:,20000:24999) = repmat(linspace(upperIe,lowerIe,5000),size(W,1),1);
-    inputSignal(:,25000:30000) = lowerIe;
+    inputSignal = ones(size(W,1),simTime)*lowerIe;
+
+    if caudInp
+
+        inputSignal(V2a_1Ix,1:3*simTime/10) = lowerIe;
+        inputSignal(V2a_1Ix,3*simTime/10:(3.5*simTime/10)-1) = repmat(linspace(lowerIe,upperIe,simTime/20),sum(V2a_1Ix),1);
+        inputSignal(V2a_1Ix,3.5*simTime/10:6.5*simTime/10) = upperIe;
+        inputSignal(V2a_1Ix,6.5*simTime/10:(7*simTime/10)-1) = repmat(linspace(upperIe,lowerIe,simTime/20),sum(V2a_1Ix),1);
+        inputSignal(V2a_1Ix,7*simTime/10:simTime) = lowerIe;
+    
+    else 
+
+        inputSignal(:,1:3*simTime/10) = lowerIe;
+        inputSignal(:,3*simTime/10:(3.5*simTime/10)-1) = repmat(linspace(lowerIe,upperIe,simTime/20),size(W,1),1);
+        inputSignal(:,3.5*simTime/10:6.5*simTime/10) = upperIe;
+        inputSignal(:,6.5*simTime/10:(7*simTime/10)-1) = repmat(linspace(upperIe,lowerIe,simTime/20),size(W,1),1);
+        inputSignal(:,7*simTime/10:simTime) = lowerIe;
+    end
+
 
     MouseNet.Simulate(simTime,'I_e', inputSignal, 'threshold', threshold);
-
-    GeneratePlot(MouseNet);
+    MouseNet.PlotRates(inputSignal)
 
 end

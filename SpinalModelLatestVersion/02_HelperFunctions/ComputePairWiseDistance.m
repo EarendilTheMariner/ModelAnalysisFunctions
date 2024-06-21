df0409dd30aef8cfaaf43222f88e15093839543c
EarendilTheMariner <august.w.i@live.dk>
2024-06-21 15:18:32 +0200

    New

--- SpinalModelLatestVersion/02_HelperFunctions/ComputePairWiseDistance.m
+++
@@ -1,9 +0,0 @@
-function DistanceMatrix = ComputePairWiseDistance(Coord)
-    P = [Coord(:,1) Coord(:,2) Coord(:,3)];
-    DistanceMatrix = zeros(length(P),length(P));
-    for i = 1:length(P)
-        for j = 1:length(P)
-            DistanceMatrix(i,j) = sqrt((P(i,1)-P(j,1)).^2 + (P(i,2)-P(j,2)).^2 + (P(i,3)-P(j,3)).^2);
-        end
-    end
-end
--- SpinalModelLatestVersion/02_HelperFunctions/RescaleConnectivity.m
+++
@@ -1,6 +0,0 @@
-function [Connectivity,Sparsity] = RescaleConnectivity(Connectivity,varargin)
-    % Rescale Connectivity
-    r = (numel(Connectivity(Connectivity>0))/numel(Connectivity(Connectivity<0)));
-    Sparsity = nnz(~Connectivity)/numel(Connectivity);
-    Connectivity = Connectivity./(sqrt(Sparsity*(1.-Sparsity)*(1.+(r^2))/2)*sqrt(length(Connectivity)));
-end
